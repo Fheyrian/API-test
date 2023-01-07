3b66086fd8bcd04bcf7196ac89239c933b5582e9
Fheyrian <mikoferdian0@gmail.com>
2023-01-08 06:34:37 +0700

    make api

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use PhpParser\Node\Stmt\Label;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -15,23 +16,6 @@ use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 require_once realpath(__DIR__ . '/..') . "/vendor/autoload.php";
 // require 'vendor/autoload.php';
 // require_once __DIR__ . "\html_tag_helper.php";
-
-// Setup some additional prefixes for DBpedia
-\EasyRdf\RdfNamespace::set('dbc', 'http://dbpedia.org/resource/Category:');
-\EasyRdf\RdfNamespace::set('dbpedia', 'http://dbpedia.org/resource/');
-\EasyRdf\RdfNamespace::set('dbo', 'http://dbpedia.org/ontology/');
-\EasyRdf\RdfNamespace::set('dbp', 'http://dbpedia.org/property/');
-
-$sparql = new \EasyRdf\Sparql\Client('http://dbpedia.org/sparql');
-
-$result = $sparql->query(
-    'SELECT * WHERE {'.
-    '  ?country rdf:type dbo:Country .'.
-    '  ?country rdfs:label ?label .'.
-    '  ?country dct:subject dbc:Member_states_of_the_United_Nations .'.
-    '  FILTER ( lang(?label) = "en" )'.
-    '} ORDER BY ?label'
-);
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -50,16 +34,54 @@ return function (App $app) {
     });
 
     $app->get('/getCountry', function (Request $request, Response $response) {
+        \EasyRdf\RdfNamespace::set('dbc', 'http://dbpedia.org/resource/Category:');
+        \EasyRdf\RdfNamespace::set('dbpedia', 'http://dbpedia.org/resource/');
+        \EasyRdf\RdfNamespace::set('dbo', 'http://dbpedia.org/ontology/');
+        \EasyRdf\RdfNamespace::set('dbp', 'http://dbpedia.org/property/');
+
+        // $sparql = new \EasyRdf\Sparql\Client('http://dbpedia.org/sparql');
+        // if($sparql==null){
+        //     $response->getBody()->write("kosong");
+        //     return $response;
+        // }
+
+        // $result = $sparql->query(
+        //     'SELECT * WHERE {' .
+        //         '  ?libray rdf:type dbo:Library .' .
+        //         '  ?library rdfs:label ?label .' .
+        //         '}'
+        // );
+
+        // $result = $sparql->query(
+        //     'SELECT * WHERE {' .
+        //         '  ?country rdf:type dbo:Country .' .
+        //         '  ?country rdfs:label ?label .' .
+        //         '  ?country dct:subject dbc:Member_states_of_the_United_Nations .' .
+        //         '  FILTER ( lang(?label) = "en" )' .
+        //         '} ORDER BY ?label'
+        // );
+
         $sparql = new \EasyRdf\Sparql\Client('http://dbpedia.org/sparql');
-        $response = $sparql->query(
-            'SELECT * WHERE {'.
-            '  ?country rdf:type dbo:Country .'.
-            '  ?country rdfs:label ?label .'.
-            '  ?country dct:subject dbc:Member_states_of_the_United_Nations .'.
-            '  FILTER ( lang(?label) = "en" )'.
-            '} ORDER BY ?label'
+        $result = $sparql->query(
+            'SELECT * WHERE {' .
+                '  ?country rdf:type dbo:Country .' .
+                '  ?country rdfs:label ?label .' .
+                '  ?country dct:subject dbc:Member_states_of_the_United_Nations .' .
+                '  FILTER ( lang(?label) = "en" )' .
+                '} ORDER BY ?label'
         );
-        // $response->getBody()->write('Hello world!');
+        $array = array();
+        foreach ($result as $row) {
+            $tempLabel = $row->label;
+            $tempURI = htmlspecialchars("$row->country");
+            $object = new stdClass();
+            $object->$tempLabel = $tempURI;
+            // $object = (object) ["$tempLabel" => "$tempURI"];
+
+            array_push($array, $object);
+        }
+
+        $response->getBody()->write(print_r($array, true));
         return $response;
     });
 };
